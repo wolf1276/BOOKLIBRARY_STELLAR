0ad1277831029b420ea7139a4060d4627779f567
wolf1276 <ahirsarkar2022@gmail.com>
2026-03-28 15:28:59 +0530

    FIX

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -4,7 +4,7 @@
 
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
